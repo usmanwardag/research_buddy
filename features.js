50bd33a3276e5479af71827f1cb390c069ab558a
Usman <usmanwardag@gmail.com>
2021-09-28 21:11:01 -0400

    Functionality for getting wiki definition

--- features.js
+++ features.js
@@ -12,19 +12,41 @@ async function searchWikipedia(searchQuery) {
 
 async function search(input) {
 
-  const inputValue = 'Fourier Transform';
+  const inputValue = 'WiFi';
   const searchQuery = inputValue.trim();
-  console.log('Trying search')
   try {
     const results = await searchWikipedia(searchQuery);
 
-    console.log(results.query.searchinfo.totalhits);
+    //console.log(results.query.searchinfo.totalhits);
 
     if (results.query.searchinfo.totalhits === 0) {
       console.log('No results found.');
     }
+    pageid = results.query.search[0].pageid;
+    console.log(pageid);
 
-    console.log(results.query.search[0].pageid);
+    const title_url = `https://en.wikipedia.org/w/api.php?action=query&pageids=${pageid}&origin=*&format=json`;
+    const title_response = await fetch(title_url);
+    const title_json = await title_response.json();
+
+    title = title_json.query.pages[pageid].title;
+    console.log(title);
+
+    const content_url = `http://en.wikipedia.org/w/api.php?action=parse&prop=text&page=${title}&origin=*&format=json`
+    const content_response = await fetch(content_url);
+    const content_json = await content_response.json();
+
+    console.log(content_json);
+
+    text = content_json.parse.text['*'];
+    text = text.toString().replace(/<[^>]*>/g, '');
+    text = text.toString().replace(/\n/g, '. ');
+    console.log(text);
+    text = text.split('.');
+    text = text[0] + '. ' + text[1] + '.';
+
+    console.log(text);
+
 
   } catch (err) {
     console.log(err);
