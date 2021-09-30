b6c02c96c3c78f43bd43497f5aa00d21f1b4f7ca
Suneha Bose <sbose2@ncsu.edu>
2021-09-30 02:10:34 -0400

    added prettier code formatter and formatted code

--- content.js
+++ content.js
@@ -6,28 +6,36 @@ document.body.appendChild(bubbleDOM);
 
 const getSelectedText = () => window.getSelection().toString();
 
-document.addEventListener("click", (e) => {
-  if (getSelectedText().length > 0) {
-    text = getSelectedText();
-    searchDefinition(text).then(summary => {
-      showBubble(e.clientX, e.clientY, summary);
-    });
-    searchPapers(text);
-    searchVideos(text);
-    searchInGit(text);
-    googleScholarSearch(text);
-  }
-}, false);
+document.addEventListener(
+	'click',
+	e => {
+		if (getSelectedText().length > 0) {
+			text = getSelectedText();
+			searchDefinition(text).then(summary => {
+				showBubble(e.clientX, e.clientY, summary);
+			});
+			searchPapers(text);
+			searchVideos(text);
+			searchInGit(text);
+			googleScholarSearch(text);
+		}
+	},
+	false
+);
 
 // hide the bubble when clicking on the screen
-document.addEventListener('mousedown', (e) => {
-  bubbleDOM.style.visibility = 'hidden';
-}, false);
+document.addEventListener(
+	'mousedown',
+	e => {
+		bubbleDOM.style.visibility = 'hidden';
+	},
+	false
+);
 
 // move the bubble to cursor location and make it visible
 function showBubble(mouseX, mouseY, html) {
-  bubbleDOM.innerHTML = html;
-  bubbleDOM.style.top = document.documentElement.scrollTop + mouseY + 'px';
-  bubbleDOM.style.left = mouseX + 'px';
-  bubbleDOM.style.visibility = 'visible';
+	bubbleDOM.innerHTML = html;
+	bubbleDOM.style.top = document.documentElement.scrollTop + mouseY + 'px';
+	bubbleDOM.style.left = mouseX + 'px';
+	bubbleDOM.style.visibility = 'visible';
 }
--- features.js
+++ features.js
@@ -1,146 +1,131 @@
-
-
 async function searchWikipedia(searchQuery) {
-  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
-  const response = await fetch(endpoint);
-  if (!response.ok) {
-    throw Error(response.statusText);
-  }
-  const json = await response.json();
-  return json;
+	const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+	const response = await fetch(endpoint);
+	if (!response.ok) {
+		throw Error(response.statusText);
+	}
+	const json = await response.json();
+	return json;
 }
 
 async function searchDefinition(input) {
+	const inputValue = input;
+	const searchQuery = inputValue.trim();
+	try {
+		const results = await searchWikipedia(searchQuery);
 
-  const inputValue = input;
-  const searchQuery = inputValue.trim();
-  try {
-    const results = await searchWikipedia(searchQuery);
+		if (results.query.searchinfo.totalhits === 0) {
+			console.log('No results found.');
+		}
+		pageid = results.query.search[0].pageid;
+		//console.log(pageid);
 
-    if (results.query.searchinfo.totalhits === 0) {
-      console.log('No results found.');
-    }
-    pageid = results.query.search[0].pageid;
-    //console.log(pageid);
+		const summary_url = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=${pageid}&origin=*`;
+		const summary_response = await fetch(summary_url);
+		const summary_json = await summary_response.json();
 
-    const summary_url = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=${pageid}&origin=*`
-    const summary_response = await fetch(summary_url);
-    const summary_json = await summary_response.json();
+		summary = summary_json.query.pages[pageid].extract;
+		summary = summary.split('.');
+		summary = summary[0] + '. ' + summary[1] + '.';
 
-    summary = summary_json.query.pages[pageid].extract;
-    summary = summary.split('.');
-    summary = summary[0] + '. ' + summary[1] + '.';
+		console.log('Summary');
+		console.log(summary);
+		return summary;
+	} catch (err) {
+		console.log('Error');
+		console.log(err);
 
-    console.log('Summary')
-    console.log(summary);
-    return summary;
-
-  } catch (err) {
-    console.log('Error');
-    console.log(err);
-
-    return '';
-  }
+		return '';
+	}
 }
 
-
 async function searchPapers(input) {
+	const inputValue = input;
+	const searchQuery = inputValue.trim();
+	try {
+		// Cross-ref API
+		// (https://www.crossref.org/documentation/retrieve-metadata/rest-api)
 
-  const inputValue = input;
-  const searchQuery = inputValue.trim();
-  try {
+		const url = `https://api.crossref.org/works?query.bibliographic="${searchQuery}"&rows=5`;
+		const response = await fetch(url);
+		const json = await response.json();
 
-    // Cross-ref API
-    // (https://www.crossref.org/documentation/retrieve-metadata/rest-api)
+		//console.log(json);
 
-    const url = `https://api.crossref.org/works?query.bibliographic="${searchQuery}"&rows=5`
-    const response = await fetch(url);
-    const json = await response.json();
+		var links = [];
 
-    //console.log(json);
+		for (let i = 0; i < 5; i++) {
+			link = json.message.items[i].URL;
+			links.push(link);
+		}
 
-    var links = []
+		console.log(links);
 
-    for (let i = 0; i < 5; i++) {
-      link = json.message.items[i].URL;
-      links.push(link);
-    }
+		return link;
+	} catch (err) {
+		console.log('Error');
+		console.log(err);
 
-    console.log(links);
-
-    return link;
-
-  } catch (err) {
-    console.log('Error');
-    console.log(err);
-
-    return '';
-  }
+		return '';
+	}
 }
 
 //searchDefinition('Fourier Transform');
 //searchPapers('SpotFi: Decimeter Level Localization Using WiFi');
 
-
 async function searchVideos(input) {
-  try {
-    const inputValue = input;
-    const searchQuery = inputValue.trim();
-    const youtubeEndpoint = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&type=video&maxResults=5&key=AIzaSyDN8OtSuFAFELEIzp8o1Rsokm957WJV_NE`
-    const response = await fetch(youtubeEndpoint);
-    const json = await response.json();
-    //console.log(json);
-    var videosResults = []
+	try {
+		const inputValue = input;
+		const searchQuery = inputValue.trim();
+		const youtubeEndpoint = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&type=video&maxResults=5&key=AIzaSyDN8OtSuFAFELEIzp8o1Rsokm957WJV_NE`;
+		const response = await fetch(youtubeEndpoint);
+		const json = await response.json();
+		//console.log(json);
+		var videosResults = [];
 
-    for (let i = 0; i < 5; i++) {
-      videoTitle = json.items[i].snippet.title;
-      videoId = json.items[i].id.videoId;
-      videoURL = `https://www.youtube.com/watch?v=${videoId}`;
-      videosResults.push(videoTitle);
-    }
+		for (let i = 0; i < 5; i++) {
+			videoTitle = json.items[i].snippet.title;
+			videoId = json.items[i].id.videoId;
+			videoURL = `https://www.youtube.com/watch?v=${videoId}`;
+			videosResults.push(videoTitle);
+		}
 
-    console.log(videosResults);
+		console.log(videosResults);
 
-    return videosResults;
-  }
-  catch (error) {
-    console.log("Error" + err.message);
-    console.log(err);
+		return videosResults;
+	} catch (error) {
+		console.log('Error' + err.message);
+		console.log(err);
 
-    return '';
-  }
-
+		return '';
+	}
 }
 async function searchInGit(input) {
-  try {
-    const inputValue = input;
-    const searchQuery = inputValue.trim();
-    const git_links = `https://github.com/search?q=${searchQuery}`
-    console.log(git_links);
-    return git_links;
-  }
-  catch (error) {
-    console.log('Error');
-    console.log(err);
+	try {
+		const inputValue = input;
+		const searchQuery = inputValue.trim();
+		const git_links = `https://github.com/search?q=${searchQuery}`;
+		console.log(git_links);
+		return git_links;
+	} catch (error) {
+		console.log('Error');
+		console.log(err);
 
-    return '';
-  }
-
+		return '';
+	}
 }
 
 async function googleScholarSearch(input) {
-  try {
-    const inputValue = input;
-    const searchQuery = inputValue.trim();
-    const gs_link = `https://scholar.google.com/scholar?hl=en&as_sdt=0%2C34&q=${searchQuery}&btnG=`
-    console.log(gs_link);
-    return gs_link;
-  }
-  catch (error) {
-    console.log('Error');
-    console.log(err);
+	try {
+		const inputValue = input;
+		const searchQuery = inputValue.trim();
+		const gs_link = `https://scholar.google.com/scholar?hl=en&as_sdt=0%2C34&q=${searchQuery}&btnG=`;
+		console.log(gs_link);
+		return gs_link;
+	} catch (error) {
+		console.log('Error');
+		console.log(err);
 
-    return '';
-  }
+		return '';
+	}
 }
-
