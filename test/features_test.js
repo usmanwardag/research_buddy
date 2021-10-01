7019adb4724c371bfcb64cccc06260263149f77d
Muskan <muskangupta7828@gmail.com>
2021-09-30 21:28:18 -0400

    Added a test case for function Search Definition

--- test/features_test.js
+++ test/features_test.js
@@ -1,33 +1,42 @@
 
 function testSearchPapers(){
-  console.log('SearchPapers test');
   searchPapers('There').then(papers => {
       if(papers.length==0)
       {
         throw "Length is zero"
       }
+  })
   searchPapers('Indoor Maps').then(papers => {
         if(papers.length==0)
           {
             throw "Length is zero"
           }
     
-      })
+  })
   searchPapers('').then(papers => {
         if(papers.length==0)
           {
             throw "Length is zero"
           }
         
-      })
+  })
   searchPapers('SE project Research Buddy').then(papers => {
         if(papers.length==0)
           {
             throw "Length is zero"
           }
         
-      })
   })
 }
 
-testSearchPapers()
+function testSearchDefinition(){
+  searchDefinition('There').then(papers => {
+      if(papers.length==0)
+      {
+        throw "Length is zero"
+      }
+  })
+}
+
+testSearchPapers();
+testSearchDefinition()
