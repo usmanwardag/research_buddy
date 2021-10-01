56aa2f04fc850d783081a1cbc5ceb67d3b6d5fd1
Suneha Bose <sbose2@ncsu.edu>
2021-09-30 20:45:34 -0400

    popup showing all contents

--- src/main.js
+++ src/main.js
@@ -16,9 +16,30 @@ document.addEventListener(
 			var wikiURL = values[1];*/
 			searchDefinition(text).then(summary => {
 				searchPapers(text).then(papers => {
-					console.log('calling showBubble');
-					console.log(summary);
-					showBubble(e.clientX, e.clientY, summary[0]);
+					searchVideos(text).then(youtubeLink => {
+						searchInGit(text).then(gitLink => {
+							searchGoogleScholar(text).then(scholarLink => {
+								searchCoursera(text).then(courseraLink => {
+									searchUdemy(text).then(udemyLink => {
+										console.log('calling showBubble');
+										console.log(summary);
+										let popupInfo = {
+											wikiDefinition: summary[0],
+											wikiURL: summary[1],
+											papers: papers,
+											youtubeLink : youtubeLink,
+											gitLink : gitLink,
+											scholarLink : scholarLink,
+											courseraLink: courseraLink,
+											udemyLink : udemyLink
+										};
+										console.log('line 36');
+										showBubble(e.clientX, e.clientY, popupInfo);
+									});
+								});
+							});
+						});
+					});
 				});
 			});
 			/*var papers = searchPapers(text);
@@ -34,13 +55,40 @@ document.addEventListener(
 );
 
 // hide the bubble when clicking on the screen
-document.addEventListener(
+/*document.addEventListener(
 	'mousedown',
 	e => {
 		bubbleDOM.style.visibility = 'hidden';
 	},
 	false
-);
+);*/
+
+function getHTML(popupInfo){
+	wikiLength = Math.min(200, popupInfo.wikiDefinition.length);
+	console.log(wikiLength);
+	wikiDefinition = String(popupInfo.wikiDefinition).substring(0,wikiLength);
+	papersLength = Math.min(5, popupInfo.papers.length);
+	console.log(papersLength);
+	
+	/*p = document.createElement("p");
+  	p.innerHTML = "DEFINITION:"+wikiDefinition;
+  	bubbleDOM.body.appendChild(p);*/
+
+	var html = `<html><body><p>${wikiDefinition}</p><a href=${popupInfo.wikiURL}>Read more</a><br><p>Papers: `;
+	for (let i = 0; i < papersLength; i++) {
+		link = popupInfo.papers[i];
+		html= html.concat(`<a href=${link}>${i+1} </a>`)
+	}
+	html = html.concat(`</p><a href=${popupInfo.youtubeLink} >YouTube </a>`);
+	html = html.concat(`<a href=${popupInfo.gitLink} >GitHub </a>`);
+	html = html.concat(`<a href=${popupInfo.udemyLink} >Udemy </a>`);
+	html = html.concat(`<a href=${popupInfo.scholarLink} >Google Scholar </a>`);
+	html = html.concat(`<a href=${popupInfo.courseraLink} >Coursera</a>`);
+	html = html.concat(`</body></html>`)
+	console.log(html);
+	return html;
+
+}
 
 /**
  * This function returns the related Udemy search results
@@ -49,7 +97,10 @@ document.addEventListener(
  * @param {string} html The detail fetched to be displayed on screen
  * @returns {string} Displays the details fetched in a pop-up on screen
  */
-function showBubble(mouseX, mouseY, html) {
+function showBubble(mouseX, mouseY, popupInfo) {
+	console.log('inside showBubble');
+	console.log(popupInfo);
+	html = getHTML(popupInfo);
 	bubbleDOM.innerHTML = html;
 	bubbleDOM.style.top = document.documentElement.scrollTop + mouseY + 'px';
 	bubbleDOM.style.left = mouseX + 'px';
